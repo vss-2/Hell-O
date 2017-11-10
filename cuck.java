a4634763a76da716e0a0dfd6430895318868108e
vss-2 <32624576+vss-2@users.noreply.github.com>
2017-11-10 15:25:17 -0300

    Update cuck.java

--- cuck.java
+++ cuck.java
@@ -29,7 +29,65 @@ public class decbin {
 				
 				int tipcadastro = in.nextInt();
 				in.nextLine();
-				if(tipcadastro==1) {}
+				if(tipcadastro==1) {
+			
+				System.out.println("Informe o nome: ");
+		String nome = in.nextLine();
+		System.out.println("Informe o preco: ");
+		double preco = in.nextDouble();
+		System.out.println("Informe a marca: ");
+		in.nextLine();
+		String marca = in.nextLine();
+		System.out.println("Informe a qualidade: ");
+		String qualidade = in.nextLine();
+
+		System.out.printf("%n1 - Calcado%n2 - Roupa%n");
+		String opcao = in.next();
+		if (opcao.equals("1")) {
+			System.out.println("Informe a cor");
+			// Como cores nao podem ser descritas usando numeros, excluimos esses casos.
+			String cor = in.next();
+			if (cor.charAt(1) == 1 ||
+					cor.charAt(1) == 2 ||	
+					cor.charAt(1) == 3 ||
+					cor.charAt(1) == 4 ||
+					cor.charAt(1) == 5 ||
+					cor.charAt(1) == 6 ||
+					cor.charAt(1) == 7 ||
+					cor.charAt(1) == 8 ||
+					cor.charAt(1) == 9) {
+				System.out.println("Cor invalida.");
+			} else {
+				System.out.printf("Procurando "+ nome + " da cor " + cor);
+			}
+			System.out.println("Informe o tamanho");
+			String tamanhoC = in.next();
+			//Como tamanhos de sapato nao podem ser diferente de numeros, nem colocamos 0 no comeco, excluimos esses casos.
+			if (tamanhoC.charAt(1) != 1 ||
+					tamanhoC.charAt(1) != 2 ||	
+					tamanhoC.charAt(1) != 3 ||
+					tamanhoC.charAt(1) != 4 ||
+					tamanhoC.charAt(1) != 5 ||
+					tamanhoC.charAt(1) != 6 ||
+					tamanhoC.charAt(1) != 7 ||
+					tamanhoC.charAt(1) != 8 ||
+					tamanhoC.charAt(1) != 9){
+				System.out.println("Procurando " + nome + "do tamanho " + tamanhoC);
+			} else {
+				System.out.print("Tamanho invalido.");
+			}
+		} else if (opcao.equals("2")) {
+			System.out.println("Informe o time: ");
+			String time = in.nextLine();
+			System.out.println("Informe a versao: ");
+			String versao = in.nextLine();
+			System.out.println("Informe o tamanho");
+			String tamanhoR = in.next();
+			System.out.printf("Procurando pelo roupa do" + time + "na versao" + versao + "do tamanho" + tamanhoR);
+		} else {
+			System.out.print("Opcao invalida.");
+		}		
+				}
 				if(tipcadastro==2) {
 				System.out.println("informe o nome do(a) cadastrado(a): ");
 				  String nome = in.nextLine();
