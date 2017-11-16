30949a38e1e79a158e5b5cfad627f8caf8f860cf
godsavemenow <32919964+godsavemenow@users.noreply.github.com>
2017-11-16 14:58:22 -0300

    Update and rename ContaNaoEncontradaException.java to Exceptions.java

--- Exceptions.java
+++ Exceptions.java
@@ -1,10 +1,4 @@
-package br.ufpe.cin.dados;
 
-/**
- * Defina a excecao ContaNaoEncontradaException no 
- * pacote br.ufpe.cin.dados
- *
- */
 public class ContaNaoEncontradaException extends Exception {
 
 	public ContaNaoEncontradaException() {
